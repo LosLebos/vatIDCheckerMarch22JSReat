41843d8700925de630e735fd17afd9e4b7ef2959
LosLebos <beneheeg@googlemail.com>
2022-03-17 21:22:41 +0100

    Several JSONs To the api

--- Office Js React/src/taskpane/components/Formular.js
+++ Office Js React/src/taskpane/components/Formular.js
@@ -16,6 +16,7 @@ const MainFormular = () => {
     }
 
     const handleButtonClick = (event) => {
+        console.log("why")
         callAPIandFillExcel(ustID)
     }
 
@@ -42,55 +43,77 @@ const MainFormular = () => {
     )
 }
 
-const callAPIandFillExcel = async ( requesterVATID) => {
+const callAPIandFillExcel = async (requesterVATID) => { // TODO 16.03. FIX this async Await. Look at the book for guidance.
+    console.log("logging-2")
     await Excel.run(async(myExcelInstance) => {
         let range = myExcelInstance.workbook.getSelectedRange();
         range.load("text");
         await myExcelInstance.sync();
-        let result;
-        let selectedVatID = range.text[0];
-        console.log(selectedVatID[0])
-        console.log(typeof selectedVatID)
-    //create the json to post
-        const ownAPIJson = JSON.stringify({
-            vatID: selectedVatID[0],
+    });
+    console.log("logging-1")
+    const selectedVatIDs = [];
+    console.log(range.text)
+    range.text.forEach((cell) => {
+        console.log("logging-3")
+        console.log(cell);
+        selectedVatIDs.push(cell.text)
+    });
+    console.log("logging-5")
+    console.log(selectedVatIDs)
+    console.log(typeof selectedVatIDs)
+//create the json to post
+    const ownAPIJsons = [];
+    selectedVatIDs.forEach((vatID) => {
+        ownAPIJsons.push(JSON.stringify({
+            vatID: vatID,
             traderName: "",
             traderCompanyType: "",
             traderStreet: "",
             traderPostcode: "",
             traderCity: "",
             requestervatID : requesterVATID
-        });
-        console.log(ownAPIJson)
-        try {
-            const response = await fetch ("https://checkvatfirst.azurewebsites.net/api/httpTriggerOne", {
-                method: "POST",
-                header:{ 'Content-Type': 'application/json' },
-                mode: "cors",
-                body: ownAPIJson
-            });
-            result = await response.json();
-            console.log(result);
-        } catch(error) {
-            console.error(error);
-            throw (error);
-        };
+        }));
+    });
+    console.log(ownAPIJsons)
+    const apiJSONResponse = await Promise.all(
+        ownAPIJsons.map(makeTheAPICall)
+    )
+        
 
-        //now write down the result on a new sheet:
-        let ws
-        try { 
-            ws = myExcelInstance.workbook.worksheets.add("VAT_IDs_by_Heegs");
-        } catch (error) {
-            console.error(error);
-            throw (error);
-        };
-        let vatIDTableHead = ws.getRange("A1:F1");
-        vatIDTableHead.values = [["VatID", "Country", "isValid", "belongsToCompany", "Adress", "ConstelationID"]];
-        vatIDTableHead.format.fill.color = "grey";
-        let vatIDTableBody = ws.getRange("A2:F2"); //TODO wrong range
-        vatIDTableBody.values = [[selectedVatID[0], result.countryCode, result.valid, result.traderName, result.traderAddress, result.requestIdentifier]];
-        return myExcelInstance.sync();
-    });  
+    //now write down the result on a new sheet:
+    let ws
+    try { 
+        ws = myExcelInstance.workbook.worksheets.add("VAT_IDs_by_Heegs");
+    } catch (error) {
+        console.error(error);
+        throw (error);
+    };
+    let vatIDTableHead = ws.getRange("A1:F1");
+    vatIDTableHead.values = [["VatID", "Country", "isValid", "belongsToCompany", "Adress", "ConstelationID"]];
+    vatIDTableHead.format.fill.color = "grey";
+    let vatIDTableBody = ws.getRange("A2:F2"); //TODO wrong range
+    let i = 0
+    vatIDTableBody.forEach((cell) => {
+        cell.text = apiJSONResponse[i];
+        i++;
+    });
+    //vatIDTableBody.values = [[selectedVatIDs[0], result.countryCode, result.valid, result.traderName, result.traderAddress, result.requestIdentifier]];
+    return myExcelInstance.sync();
+};
+
+async function makeTheAPICall(apiJSON) {
+    try {
+        let response = await fetch ("https://checkvatfirst.azurewebsites.net/api/httpTriggerOne", {
+            method: "POST",
+            header:{ 'Content-Type': 'application/json' },
+            mode: "cors",
+            body: apiJSON
+        });
+        return await response.json();
+    } catch(error) {
+        console.error(error);
+        throw (error);
+    };
 }
 
 export { MainFormular }
