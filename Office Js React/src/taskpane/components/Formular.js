2cf2f99c36d1e2eec7431fb8c8e183801435af5d
LosLebos <beneheeg@googlemail.com>
2022-03-24 18:10:43 +0100

    Formating and Errorhandling

--- Office Js React/src/taskpane/components/Formular.js
+++ Office Js React/src/taskpane/components/Formular.js
@@ -41,8 +41,8 @@ const MainFormular = () => {
     )
 }
 
-const callAPIandFillExcel = async (requesterVATID) => { // TODO 16.03. FIX this async Await. Look at the book for guidance.
-    await Excel.run(async(myExcelInstance) => {
+const callAPIandFillExcel = async (requesterVATID) => { 
+    await Excel.run(async(myExcelInstance) => { //this probably shit to execute all the code within this await???
         let range = myExcelInstance.workbook.getSelectedRange();
         range.load("text");
         const worksheets = myExcelInstance.workbook.worksheets; //used later to determine name of new sheet
@@ -52,7 +52,7 @@ const callAPIandFillExcel = async (requesterVATID) => { // TODO 16.03. FIX this 
         const selectedVatIDs = range.text
     //create the json to post
         const ownAPIJsons = [];
-         // 17.03. wird irgendwie nicht angezeigt. Ausserdem ist VatID nächste Zeile irgendwie nen Tupel und die API mag das nicht.
+        
         selectedVatIDs.forEach((vatID) => {
             
             ownAPIJsons.push(JSON.stringify({
@@ -65,16 +65,29 @@ const callAPIandFillExcel = async (requesterVATID) => { // TODO 16.03. FIX this 
                 requestervatID : requesterVATID
             }));
         });
-        console.log("ownJSONAPI", ownAPIJsons);
-        console.log("the other way: ",JSON.stringify(ownAPIJsons));
         ///const apiJSONResponse = await Promise.all( /// this is pretty cool, but i dont want to send every line by itself.
          //   ownAPIJsons.map(makeTheAPICall)
         //)
-        const apiJSONResponse = await makeTheAPICall(ownAPIJsons)
+
+        //TODO insert handling non 200
         
-        console.log("first apiJson response", apiJSONResponse[0])
+        const apiResponse = await makeTheAPICall(ownAPIJsons)
+        const apiStatusResponse = await apiResponse.status;
+        let apiJSONResponse;
+        if (apiStatusResponse == 200 || apiStatusResponse == 201) {
+            apiJSONResponse = await apiResponse.json();
+        } else {
+           //apiJSONResponse = await apiResponse.json(); //TODO i still get the response here, because iam an idiot and do not know how to throw an error
+            throw "The API returned an Error with STatus Code " + String(apiStatusResponse)
+        }
+        
+        
+        
         //now write down the result on a new sheet:
+        
         let ws
+        let wsCreated = false;
+        let numberOfRuns = 0;
         const worksheetNames = [];
         try { 
             worksheets.items.forEach((worksheet => {
@@ -83,34 +96,40 @@ const callAPIandFillExcel = async (requesterVATID) => { // TODO 16.03. FIX this 
             for (let i = 0; i < 10; i++) {
                 if (! worksheetNames.includes("VAT_IDs_by_Heegs_" + String(i))) {
                     ws = myExcelInstance.workbook.worksheets.add("VAT_IDs_by_Heegs_" + String(i));
+                    numberOfRuns = String(i);
+                    wsCreated = true;
                     break;
                 };
-            //TODO warning more than 10 sheets
+            if (wsCreated = false) {
+                ws = myExcelInstance.workbook.worksheets.add() //Excel determines the name.
             }
-            
-
+            }
         } catch (error) {
             console.error(error);
             throw (error);
         };
-        let vatIDTableHead = ws.getRange("A1:F1");
-        vatIDTableHead.values = [["VatID", "Country", "isValid", "TraderName", "Address", "ConstelationID"]];
-        vatIDTableHead.format.fill.color = "grey";
-        let lengthOfReturn = apiJSONResponse.length
-        for (let i = 0; i < lengthOfReturn; i++) {
-            console.log({i})
-            let currentRow = i + 2
-            let vatIDTableBodyRow = ws.getRange("A" + String(currentRow) + ":F" + String(currentRow)); 
-            let thisRowValues = [[apiJSONResponse[i].countryCode + apiJSONResponse[i].vatNumber, apiJSONResponse[i].countryCode, apiJSONResponse[i].valid, apiJSONResponse[i].traderName, apiJSONResponse[i].traderAddress, apiJSONResponse[i].requestIdentifier]];
-            vatIDTableBodyRow.values = thisRowValues
-            vatIDTableBodyRow.format.fill.color = "green";
-            console.log("thisRowValues" , thisRowValues);
-            await myExcelInstance.sync();
+
+        //create a table for this.
+        let returnTable = ws.tables.add("A1:F1", true);
+        if (wsCreated) { //you cannot name two talbes the same.
+            returnTable.name = "VAT_IDs_by_Heegs" + numberOfRuns;
         }
         
+        returnTable.getHeaderRowRange().values = [["VatID", "Country", "isValid", "TraderName", "Address", "ConstelationID"]];
+        //returnTable.getHeaderRowRange().format.fill.color = "grey";
+        
+        let lengthOfReturn = apiJSONResponse.length;
+        for (let i = 0; i < lengthOfReturn; i++) {
+            let thisRowValues = [[apiJSONResponse[i].countryCode + apiJSONResponse[i].vatNumber, apiJSONResponse[i].countryCode, apiJSONResponse[i].valid, apiJSONResponse[i].traderName, apiJSONResponse[i].traderAddress, apiJSONResponse[i].requestIdentifier]];
+            returnTable.rows.add(null, thisRowValues);
+            
+        };
+        console.log("end")
+        ws.getUsedRange().format.autofitColumns();
+        ws.getUsedRange().format.autofitRows();
 
     }).catch(function (error) {
-        console.log("Error: " + error)
+        console.log("Error: " + error +  " +++++ " + error.stack)
         throw (error);
     });
     
@@ -118,17 +137,14 @@ const callAPIandFillExcel = async (requesterVATID) => { // TODO 16.03. FIX this 
 };
 
 async function makeTheAPICall(apiJSON) {
-    console.log({apiJSON})
-    console.log(typeof apiJSON)
     try {
         let response = await fetch ("https://checkvatfirst.azurewebsites.net/api/httpTriggerOne", {
             method: "POST",
             header:{ 'Content-Type': 'application/json' },
-            mode: "cors",
+            mode: "cors", //i could make this better and safer, not using cors but another backend to call the api
             body: JSON.stringify(apiJSON)
         });
-        //console.log("response: " , await response.json())
-        return await response.json();
+        return response; //this just returns the promise, you have to await it to use.
         
     } catch(error) {
         console.error(error);
